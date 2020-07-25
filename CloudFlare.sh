4a603867e7ccd1e06d9fcfedbe0bc1f5b8e8bf5b
TheKito <11139894+TheKito@users.noreply.github.com>
2020-07-25 16:23:28 -0300

    Update CloudFlare.sh

--- CloudFlare.sh
+++ CloudFlare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IPTABLES=$(which "iptables")
-wget -q https://www.cloudflare.com/ips-v4 -O - | while read ip
+wget --inet4-only -q https://www.cloudflare.com/ips-v4 -O - | while read ip
 do
         $IPTABLES -A INPUT  -p tcp --dport 80  -s $ip -j ACCEPT
         $IPTABLES -A INPUT  -p tcp --dport 443 -s $ip -j ACCEPT
