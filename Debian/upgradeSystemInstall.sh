b5b69bd6f2f696c25783fac422bc2bc6240325d5
TheKito <11139894+TheKito@users.noreply.github.com>
2020-08-13 18:40:59 -0300

    Update upgradeSystemInstall.sh

--- Debian/upgradeSystemInstall.sh
+++ Debian/upgradeSystemInstall.sh
@@ -7,15 +7,6 @@ deb http://security.debian.org/debian-security stable/updates main
 deb-src http://security.debian.org/debian-security stable/updates main
 " > /etc/apt/sources.list
 
-export DEBIAN_FRONTEND=noninteractive;
-
-apt-get update
-apt-get upgrade -dy
-apt-get dist-upgrade -dy
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get autoremove -y
-
 mkdir -p /Data/Scripts/runWeekly/ 
 
 echo "
@@ -35,4 +26,4 @@ test -f /var/run/reboot-required && reboot
 " > /Data/Scripts/runWeekly/upgradeSystem.sh
 
 chmod +x /Data/Scripts/runWeekly/upgradeSystem.sh
-test -f /var/run/reboot-required && reboot
+/Data/Scripts/runWeekly/upgradeSystem.sh
