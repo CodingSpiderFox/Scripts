f05ecd3e2c0ba03293ef86a43b8d1aff5453c630
TheKito <11139894+TheKito@users.noreply.github.com>
2020-08-13 15:31:12 -0300

    Create upgradeSystem.sh

---
+++ Debian/upgradeSystem.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+echo "
+deb http://deb.debian.org/debian/ stable main
+deb-src http://deb.debian.org/debian/ stable main
+
+deb http://security.debian.org/debian-security stable/updates main
+deb-src http://security.debian.org/debian-security stable/updates main
+" > /etc/apt/sources.list
+
+export DEBIAN_FRONTEND=noninteractive;
+
+apt-get update
+apt-get upgrade -dy
+apt-get dist-upgrade -dy
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+
+test -f /var/run/reboot-required && reboot
