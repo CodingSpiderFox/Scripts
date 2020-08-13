0072ae5b3c64f8528823295e94835a65cc0c3fd0
TheKito <11139894+TheKito@users.noreply.github.com>
2020-08-13 18:38:50 -0300

    Create upgradeSystemInstall.sh

---
+++ Debian/upgradeSystemInstall.sh
@@ -0,0 +1,38 @@
+#/bin/bash
+
+echo "
+deb http://deb.debian.org/debian/ stable main
+deb-src http://deb.debian.org/debian/ stable main
+deb http://security.debian.org/debian-security stable/updates main
+deb-src http://security.debian.org/debian-security stable/updates main
+" > /etc/apt/sources.list
+
+export DEBIAN_FRONTEND=noninteractive;
+
+apt-get update
+apt-get upgrade -dy
+apt-get dist-upgrade -dy
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+
+mkdir -p /Data/Scripts/runWeekly/ 
+
+echo "
+#/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive;
+
+apt-get update
+apt-get upgrade -dy
+apt-get dist-upgrade -dy
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get autoremove -y
+
+test -f /var/run/reboot-required && reboot
+
+" > /Data/Scripts/runWeekly/upgradeSystem.sh
+
+chmod +x /Data/Scripts/runWeekly/upgradeSystem.sh
+test -f /var/run/reboot-required && reboot
