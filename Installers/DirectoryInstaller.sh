966568f57d8d07d6828b323060bac17b6c6be556
TheKito <11139894+TheKito@users.noreply.github.com>
2020-10-10 23:37:37 -0300

    Update DirectoryInstaller.sh

--- Installers/DirectoryInstaller.sh
+++ Installers/DirectoryInstaller.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-mkdir -p /opt/blk/scripts/run/minutely
-mkdir -p /opt/blk/scripts/run/hourly
-mkdir -p /opt/blk/scripts/run/daily
-mkdir -p /opt/blk/scripts/run/weekly
-mkdir -p /opt/blk/scripts/run/monthly
-mkdir -p /opt/blk/scripts/run/yearly
+mkdir -p /opt/kito/scripts/run/minutely
+mkdir -p /opt/kito/scripts/run/hourly
+mkdir -p /opt/kito/scripts/run/daily
+mkdir -p /opt/kito/scripts/run/weekly
+mkdir -p /opt/kito/scripts/run/monthly
+mkdir -p /opt/kito/scripts/run/yearly
+mkdir -p /opt/kito/scripts/run/startup
 
-cat /etc/crontab | grep "/opt/blk/scripts/run/minutely"     || echo "* *    * * *   root    /Data/Scripts/runDirectory.sh /opt/blk/scripts/run/minutely" >> /etc/crontab
-cat /etc/crontab | grep "/opt/blk/scripts/run/hourly"       || echo "$(shuf -i 0-59 -n 1) * * * *   root    /Data/Scripts/runDirectory.sh /opt/blk/scripts/run/hourly" >> /etc/crontab
-cat /etc/crontab | grep "/opt/blk/scripts/run/daily"        || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      * * *   root    /Data/Scripts/runDirectory.sh /opt/blk/scripts/run/daily" >> /etc/crontab
-cat /etc/crontab | grep "/opt/blk/scripts/run/weekly"       || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      * * $(shuf -i 0-6 -n 1) root    /Data/Scripts/runDirectory.sh /opt/blk/scripts/run/weekly" >> /etc/crontab
-cat /etc/crontab | grep "/opt/blk/scripts/run/monthly"      || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      $(shuf -i 1-28 -n 1) * *        root    /Data/Scripts/runDirectory.sh /opt/blk/scripts/run/monthly" >> /etc/crontab
-cat /etc/crontab | grep "/opt/blk/scripts/run/yearly"       || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      $(shuf -i 1-28 -n 1) $(shuf -i 1-12 -n 1) *     root   /Data/Scripts/runDirectory.sh /opt/blk/scripts/run/yearly" >> /etc/crontab
+cat /etc/crontab | grep "/opt/kito/scripts/run/minutely"     || echo "* *    * * *   root    /Data/Scripts/runDirectory.sh /opt/kito/scripts/run/minutely" >> /etc/crontab
+cat /etc/crontab | grep "/opt/kito/scripts/run/hourly"       || echo "$(shuf -i 0-59 -n 1) * * * *   root    /Data/Scripts/runDirectory.sh /opt/kito/scripts/run/hourly" >> /etc/crontab
+cat /etc/crontab | grep "/opt/kito/scripts/run/daily"        || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      * * *   root    /Data/Scripts/runDirectory.sh /opt/kito/scripts/run/daily" >> /etc/crontab
+cat /etc/crontab | grep "/opt/kito/scripts/run/weekly"       || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      * * $(shuf -i 0-6 -n 1) root    /Data/Scripts/runDirectory.sh /opt/kito/scripts/run/weekly" >> /etc/crontab
+cat /etc/crontab | grep "/opt/kito/scripts/run/monthly"      || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      $(shuf -i 1-28 -n 1) * *        root    /Data/Scripts/runDirectory.sh /opt/kito/scripts/run/monthly" >> /etc/crontab
+cat /etc/crontab | grep "/opt/kito/scripts/run/yearly"       || echo "$(shuf -i 0-59 -n 1) $(shuf -i 0-23 -n 1)      $(shuf -i 1-28 -n 1) $(shuf -i 1-12 -n 1) *     root   /Data/Scripts/runDirectory.sh /opt/kito/scripts/run/yearly" >> /etc/crontab
 
 echo "#!/bin/bash
 
@@ -23,6 +24,6 @@ for i in \$(find \"\$1\" -type f -name \"*.sh\"); do
         fi
 done
 
-" > /opt/blk/scripts/runDirectory.sh
+" > /opt/kito/scripts/runDirectory.sh
 
-chmod +x /opt/blk/scripts/runDirectory.sh
+chmod +x /opt/kito/scripts/runDirectory.sh
